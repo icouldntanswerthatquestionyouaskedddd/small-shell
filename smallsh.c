9c4df8f17b43f8956b1a88a76d3f16ba4f46d6b6
icouldntanswerthatquestionyouaskedddd <wengn@os1.engr.oregonstate.edu>
2026-05-05 20:40:00 -0700

    oj

--- smallsh.c
+++ smallsh.c
@@ -11,26 +11,88 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+struct commandData
+{
+    char *command;
+    char *args[512];
+    char *input_file;
+    char *output_file;
+    bool background;
+};
+
+/*
+* Parse a given command line string and fill the fields in a
+* commandData struct with corresponding values from the command
+* line
+*/
+void parseCommandLine(char *commandLine, struct commandData *shellCommand)
+{
+    ssize_t len = 0;
+    ssize_t nread;
+    char *token;
+    
+    // Parse the first token, the starting command
+    token = strtok(commandLine, " \n");
+    if (!token)
+    {
+        shellCommand->command = NULL;
+    }
+    else
+    {
+        shellCommand->command = calloc(strlen(token) + 1, sizeof(char));
+        strcpy(shellCommand->command, token);
+    }
+    
+}
+
+/*
+* Kill all other processes or jobs started by the shell
+*/
 void killAll()
 {
 
 }
 
+/*
+* Provides the colon : prompt prepetually until "exit" command is entered,
+* and checks for the three built-in commands before parsing the command line
+* for other commands. Invokes other functions to take actions based on
+* those commands. Creates structs for the command lines for other commands.
+*/
 int main()
 {
     bool exit = false;
+
+    // Check of the three built-in commands (exit, cd, and status) first.
     while (!exit)
     {
         printf(": ");
         fflush(NULL);
-        char *commandLine;
+        char *commandLine = NULL;
         ssize_t len = 0;
         ssize_t nread = getline(&commandLine, &len, stdin);
-        if (strcmp(commandLine, "exit\n") == 0)
+        struct commandData *shellCommand = malloc(sizeof(struct commandData));
+        parseCommandLine(commandLine, shellCommand);
+        if (!shellCommand->command)
+        {
+            continue;
+        }
+        else if (strcmp(shellCommand->command, "exit") == 0)
         {
             bool exit = true;
             break;
         }
+        else if (strcmp(shellCommand->command, "cd") == 0)
+        {
+
+            continue;
+        }
+        else if (strcmp(shellCommand->command, "status") == 0)
+        {
+
+            continue;
+        }
+        
     }
     killAll();
     return EXIT_SUCCESS;
